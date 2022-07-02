bcfabbdc83f44bcfa3125ed7de360e011e2351ef
ibnahmadbello <justforlearningcode@gmail.com>
2022-07-02 15:31:38 +0100

    Rewrote the path for the controller

--- src/main/java/com/arab/controller/TaskController.java
+++ src/main/java/com/arab/controller/TaskController.java
@@ -3,16 +3,15 @@ package com.arab.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arab.task.entity.Task;
 import com.arab.task.repo.TaskRepository;
 
-@RestController
+@Controller
 public class TaskController {
 	
 	@Autowired
